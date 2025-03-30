925d33f5870e632a50a07772ddb7b3144aa0e8f3
Arc-001 <25131347+Arc-001@users.noreply.github.com>
2025-03-30 23:55:40 +0530

    Added the insert blog and insert img

--- backend/model/database.py
+++ backend/model/database.py
@@ -1,14 +1,10 @@
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
 import io
 from PIL import Image
-import base64
 import datetime
-
-
 
 class DB_blog:
     def __init__(self):
@@ -59,6 +55,32 @@ class DB_blog:
 
 
         return str(self.img_coll.insert_one(img_dic))
+    
+    def blog_dictionary_construction(self, title:str, content:str, img_id:list[str], writting_date:datetime.datetime) -> dict:
+
+        #formatting and making the dictionary that will be later inserted to
+        # to the blog collection in the database
+
+        blog_dic = {
+            "title":title,
+            "content":content,
+            "img_id":img_id,
+            "writting_date":writting_date,
+            "timestamp_of_insertion":datetime.datetime.now()
+        }
+
+
+        return blog_dic
+    
+    def insert_blog(self,**kwargs) -> str:
+        if ["image_id", "title", "content", "writting_date"] in kwargs:
+            blog_dic = self.blog_dictionary_construction(kwargs["title"], kwargs["content"], kwargs["image_id"], kwargs["writting_date"])
+        elif blog_dic in kwargs:
+            blog_dic = kwargs["blog_dic"]
+        else:
+            raise ValueError("Please provide the blog dictionary or required fields.")
+        
+        
 
 
 
